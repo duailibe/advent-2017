3ed660a322050e637d5094b512fd82c69ac38f02
Lucas Duailibe <lucasds@gmail.com>
2017-12-04 11:44:59 -0200

    day 4

---
+++ day4/solution.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const path = require("path");
+
+const INPUT = fs
+  .readFileSync(path.join(__dirname, "input"), "utf-8")
+  .split("\n");
+
+function isValid1(passphrase) {
+  const words = passphrase.split(/\s+/).sort();
+  for (var i = 1; i < words.length; i++) {
+    if (words[i - 1] === words[i]) return false;
+  }
+  return true;
+}
+
+function isValid2(passphrase) {
+  let words = passphrase
+    .split(/\s+/)
+    .map(w =>
+      w
+        .split("")
+        .sort()
+        .join("")
+    )
+    .sort();
+  for (var i = 1; i < words.length; i++) {
+    if (words[i - 1] === words[i]) return false;
+  }
+  return true;
+}
+
+function count(check) {
+  return INPUT.reduce(
+    (count, passphrase) =>
+      passphrase && check(passphrase) ? count + 1 : count,
+    0
+  );
+}
+
+console.log("First half answer:", count(isValid1));
+console.log("First half answer:", count(isValid2));
